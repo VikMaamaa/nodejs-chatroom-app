03453128fc3d25980f0836cf072ba826b7261eb6
vikmaamaa <maamaavictor@yahoo.com>
2021-08-24 17:00:40 +0100

    final build

--- server.js
+++ server.js
@@ -65,8 +65,8 @@ io.on('connection', socket => {
     })
 })
 
-const PORT = 3000 || process.env.PORT
+var PORT = 3000 || process.env.PORT
 
 server.listen(PORT, () => {
-    console.log(`Server started on port`);
+    console.log(`Server started on port ${PORT}`);
 });
